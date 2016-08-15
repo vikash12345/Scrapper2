5545fec09861efe1bd28a41929e7f310f257cb9e
vikash12345 <vikash@backgroundcheck.email>
2016-08-15 14:47:00 +0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,15 +1,15 @@
 # This is a template for a Python scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
- import scraperwiki
- import lxml.html
+import scraperwiki
+import lxml.html
 #
 # # Read in a page
  html = scraperwiki.scrape("https://en.wikipedia.org/wiki/List_of_state_and_union_territory_capitals_in_India")
 #
 # # Find something on the page using css selectors
  root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
+root.cssselect("div[align='left']")
 #
 # # Write out to the sqlite database using scraperwiki library
 # scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
