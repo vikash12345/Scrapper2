c1a33ab14459607aa0a432d833a4a0c98ba7735b
vikash12345 <vikash@backgroundcheck.email>
2016-08-15 14:44:10 +0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,14 +1,14 @@
 # This is a template for a Python scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# import scraperwiki
-# import lxml.html
+ import scraperwiki
+ import lxml.html
 #
 # # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
+ html = scraperwiki.scrape("https://en.wikipedia.org/wiki/List_of_state_and_union_territory_capitals_in_India")
 #
 # # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
+ root = lxml.html.fromstring(html)
 # root.cssselect("div[align='left']")
 #
 # # Write out to the sqlite database using scraperwiki library
@@ -23,12 +23,5 @@
 # called "data.sqlite" in the current working directory which has at least a table
 # called "data".
 
-
-import urllib2
-import scraperwiki
-import lxml.html
-wiki = "https://en.wikipedia.org/wiki/List_of_state_and_union_territory_capitals_in_India"
-page = urllib2.urlopen(wiki)
-soup = BeautifulSoup(page)
 
 
