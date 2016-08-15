2f0c332f1d44d7aae5efb6589f2a956353a2cd68
vikash12345 <vikash@backgroundcheck.email>
2016-08-15 14:41:14 +0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -22,3 +22,12 @@
 # All that matters is that your final data is written to an SQLite database
 # called "data.sqlite" in the current working directory which has at least a table
 # called "data".
+
+
+import urllib2
+from bs4 import BeautifulSoup
+wiki = "https://en.wikipedia.org/wiki/List_of_state_and_union_territory_capitals_in_India"
+page = urllib2.urlopen(wiki)
+soup = BeautifulSoup(page)
+
+
